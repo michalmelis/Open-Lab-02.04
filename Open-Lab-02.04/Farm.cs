a42a720c3ff24dbbe43fe5dfa2117bde20c5702c
michalmelis <michalmelis.sk@gmail.com>
2019-11-02 20:42:09 +0100

    Done.

--- Open-Lab-02.04/Farm.cs
+++ Open-Lab-02.04/Farm.cs
@@ -6,6 +6,7 @@ namespace Open_Lab_02._04
     {
         public int GetLegsCount(int chickens, int cows, int pigs)
         {
+            return ((chickens * 2) + (cows * 4) + (pigs * 4));
             throw new NotImplementedException();
         }
     }
